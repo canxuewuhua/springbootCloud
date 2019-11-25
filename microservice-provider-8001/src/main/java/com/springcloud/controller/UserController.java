f2e6a376343cf6e30d67afa2442e7661baa2bbe2
yongqiang.zhu <981162250@qq.com>
2019-11-26 01:30:05 +0800

    服务发现的接口

--- microservice-provider-8001/src/main/java/com/springcloud/controller/UserController.java
+++ microservice-provider-8001/src/main/java/com/springcloud/controller/UserController.java
@@ -2,10 +2,15 @@ package com.springcloud.controller;
 
 import com.springcloud.common.ResultDTO;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author yongqiang.zhu
@@ -13,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/provider")
 public class UserController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	/**
 	 *  用户信息
@@ -27,5 +35,26 @@ public class UserController {
 		resultDTO.setMessage("成功！");
 		resultDTO.setData("用户信息");
 		return resultDTO;
+	}
+	/**
+	 *  服务发现
+	 * @return
+	 */
+	@ApiOperation(value="服务发现", notes="服务发现")
+	@RequestMapping(name = "服务发现", path = "/discovery",method = RequestMethod.GET)
+	@ResponseBody
+	public Object discovery(){
+		List<String> services = discoveryClient.getServices();
+		System.out.println("发现的服务如：" + services);
+		List<ServiceInstance> list = discoveryClient.getInstances("MICROSERVICE-PROVIDER-USER");
+		for (ServiceInstance serviceInstance : list){
+			System.out.println(
+					serviceInstance.getHost()+"\t"+
+					serviceInstance.getPort()+"\t"+
+					serviceInstance.getUri()+"\t"+
+					serviceInstance.getServiceId()
+			);
+		}
+		return this.discoveryClient;
 	}
 }
